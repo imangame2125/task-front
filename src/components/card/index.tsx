d86a7c15462f45b03a029dd100ae6086c2e48384
iman majd <imanclever51@gmail.com>
2024-09-25 20:35:21 +0330

    chart not correct

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -11,40 +11,40 @@ interface CardProps {
     period3: string
   }
   imageUrl: string
-  label1: string
-  label2: string
+  label1?: string
+  label2?: string
 }
 
 const Card: FC<CardProps> = ({ title, description, timePeriods, imageUrl, label1, label2 }) => {
   return (
-    <div className='w-72 h-[400px] mx-4 cursor-pointer bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden'>
+    <div className='mx-4 cursor-pointer bg-white border border-gray-200 rounded-lg shadow-md'>
       <div className='p-4'>
         <div className='flex justify-between items-center'>
           <p className='text-sm font-bold text-[#242424]'>{title}</p>
           <IconElement icon={More} className='text-[#424242] w-5 h-5' />
         </div>
         <p className='text-[#616161] font-normal text-xs mt-1'>{description}</p>
-        <div className='flex space-x-4 mt-2 items-center ml-5'>
+        <div className='flex gap-x-4  items-center ml-5'>
           <p className='text-[#242424] border-b-2 text-sm border-[#5B5FC7]'>
-            {timePeriods?.period1 || ''}
+            {timePeriods?.period1 || <div className='mt-4'></div>}
           </p>
           <p className='text-[#424242] text-sm'>{timePeriods?.period2 || ''}</p>
           <p className='text-[#424242] text-sm'>{timePeriods?.period3 || ''}</p>
         </div>
-        <div className='w-full flex justify-center items-center mt-14'>
-          <img src={imageUrl} alt='card-img' />
+        <div className='w-full flex justify-center items-center'>
+          <img className='' src={imageUrl} alt='card-img' />
         </div>
-        <div className='flex items-center space-x-2 justify-center mt-9'>
+        <div className='flex items-center gap-x-2 justify-center'>
           <span className='bg-[#00B7C3] text-blue-800 text-xs font-medium w-2.5 h-2.5 '></span>
           <p className='text-xs text-[#424242]'>{label1}</p>
           <span className='bg-[#D1D1D1] text-xs font-medium w-2.5 h-2.5'></span>
           <p className='text-xs text-[#424242]'>{label2}</p>
         </div>
-      </div>
-      <div className='mt-16'>
-        <button className='text-[#5B5FC7] flex font-medium rounded-lg text-sm px-5 text-center pb-4'>
-          View Details
-        </button>
+        <div className='mt-16'>
+          <button className='text-[#5B5FC7] flex font-medium rounded-lg text-sm px-5 text-center pb-4'>
+            View Details
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/pages/vanArdsel/pages/Home/index.tsx
+++ src/pages/vanArdsel/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import Filter from './components/filter'
 import Tree, { TreeNode } from '@/components/tree'
 import Card from '@/components/card'
 import CardOne from '@/assests/images/card-img.svg'
+import CardTwo from '@/assests/images/bar-chart.png'
+import CardThree from '@/assests/images/Chart 3.svg'
 const treeDataMock: TreeNode[] = [
   {
     title: 'Group title',
@@ -57,32 +59,60 @@ const Home = () => {
   return (
     <>
       <Filter />
-      <div className='w-full flex gap-x-2'>
-        <div className='w-48 h-screen border border-[#E0E0E0]'>
+      <div className='w-full h flex gap-x-2 h-screen '>
+        <div className='w-48 border border-[#E0E0E0]'>
           <Tree data={treeDataMock} expandedKeys={expandedKeys} onNodeSelect={handleNodeSelect} />
         </div>
-        <div className='w-full flex mt-5'>
-          <div className='w-1/3 '>
+        <div className='w-full'>
+          <div className='w-full py-2 flex mt-5'>
+            <div className='flex'>
+              <Card
+                label2='Label 2'
+                timePeriods={{
+                  period1: '7 days',
+                  period2: '30  days',
+                  period3: '60 days',
+                }}
+                description='Description'
+                title='Card title'
+                imageUrl={CardOne}
+                label1='Label 1'
+              />
+            </div>
+            <div className=''>
+              <Card
+                label2='Label 2'
+                description='Description'
+                title='Card title'
+                imageUrl={CardTwo}
+                label1='Label 1'
+              />
+            </div>
+            <div className='w-3/4'>
+              <Card
+                description='Description'
+                title='Card title'
+                imageUrl={CardThree}
+                timePeriods={{
+                  period1: '7 days',
+                  period2: '30  days',
+                  period3: '60 days',
+                }}
+              />
+            </div>
+          </div>
+          <div className='w-1/3'>
             <Card
-              label2='Label 2'
+              description='Description'
+              title='Card title'
+              imageUrl={CardThree}
               timePeriods={{
                 period1: '7 days',
                 period2: '30  days',
                 period3: '60 days',
               }}
-              description='Description'
-              title='Card title'
-              imageUrl={CardOne}
-              label1='Label 1'
             />
           </div>
-          <Card
-            label2='Label 2'
-            description='Description'
-            title='Card title'
-            imageUrl={CardOne}
-            label1='Label 1'
-          />
         </div>
       </div>
     </>
